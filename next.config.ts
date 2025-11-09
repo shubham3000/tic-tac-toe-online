ff6eabf5ae90d6c1a516ea8eb28bca57029ea46f
shubham3000 <shubhamsadhu02@gmail.com>
2025-11-09 21:24:54 +0530

    update config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  pageExtensions: ["tsx", "ts", "jsx", "js"],
+  output: "standalone",
 };
 
 export default nextConfig;
